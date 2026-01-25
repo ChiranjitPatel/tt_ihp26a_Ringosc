df40af48c0fc6a0b6180afde120f80149d06c2f8
chiranjitpatel <chiranjitpatel08@gmail.com>
2026-01-25 18:02:23 +0530

    test1

---
+++ src/tt_um_ro.sv
@@ -0,0 +1,18 @@
+module tt_um_ro (
+	input logic enable,
+    output logic osc_out
+);
+
+    (* keep = "true", dont_touch = "true" *) logic a, b, c;
+    // wire startup = 1'b0;  // Constant 0
+	
+	nand X1 (a, c ,enable);
+    // assign #2 a = startup ? 1'b0 : ~c;
+    // assign #2 a = ~c;
+    assign #100ps b = ~a;
+    assign #100ps c = ~b;  // Forces c=0 only at start (but simplified)
+
+	// pulldown (c);  // Weak pull-down to 0
+    assign osc_out = c;
+
+endmodule

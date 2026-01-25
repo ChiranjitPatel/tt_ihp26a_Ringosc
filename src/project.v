ec601c17f16ece7057ad027a5b6715a47d5b99cb
chiranjitpatel <chiranjitpatel08@gmail.com>
2026-01-25 18:41:54 +0530

    t9

--- src/project.v
+++ src/project.v
@@ -11,9 +11,9 @@ module tt_um_ro (
     output wire [7:0] uo_out,   // Dedicated outputs
     input  wire       ena,      // always 1 when the design is powered, so you can ignore it
     input  wire       clk,      // clock
-    input  wire       rst_n     // reset_n - low to reset
+    input  wire       rst_n,    // reset_n - low to reset
     output wire [7:0] uio_out,  // IOs: Output path
-    output wire [7:0] uio_oe,   // IOs: Enable path (active high: 0=input, 1=output)
+    output wire [7:0] uio_oe   // IOs: Enable path (active high: 0=input, 1=output)
     
 );
 

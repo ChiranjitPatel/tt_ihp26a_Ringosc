f2a6e181757641b448a789387c80637dc10206ce
chiranjitpatel <chiranjitpatel08@gmail.com>
2026-01-26 22:12:09 +0530

    removed timing delay in ringosc.sv

--- src/ringosc.sv
+++ src/ringosc.sv
@@ -3,15 +3,15 @@ module ringosc (
     output osc_out
 );
 
-    logic a, b, c;
-    // (* keep = "true", dont_touch = "true" *) logic a, b, c;
+    // logic a, b, c;
+    (* keep = "true", dont_touch = "true" *) logic a, b, c;
     // wire startup = 1'b0;  // Constant 0
 	
 	nand X1 (a, c ,enable);
     // assign #2 a = startup ? 1'b0 : ~c;
     // assign #2 a = ~c;
-    assign #100ps b = ~a;
-    assign #100ps c = ~b;  // Forces c=0 only at start (but simplified)
+    assign b = ~a;
+    assign c = ~b;  // Forces c=0 only at start (but simplified)
 
 	// pulldown (c);  // Weak pull-down to 0
     assign osc_out = c;

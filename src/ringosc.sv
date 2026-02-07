88e077cc49a1b85ef22307c814fa297f2b939617
chiranjitpatel <chiranjitpatel08@gmail.com>
2026-02-07 14:32:54 +0530

    added comb logic NAND gate

--- src/ringosc.sv
+++ src/ringosc.sv
@@ -11,11 +11,12 @@ module ringosc (
     // (* keep = "true", dont_touch = "true" *) logic a, b, c;
     // wire startup = 1'b0;  // Constant 0
 	
-	nand X1 (a, c ,enable);
+	//nand X1 (a, c ,enable);
+	assign a = !(c & enable)
     // assign #2 a = startup ? 1'b0 : ~c;
     // assign #2 a = ~c;
     assign b = ~a;
-    assign c = ~b;  // Forces c=0 only at start (but simplified)
+    assign c = ~b; 
 
 	// pulldown (c);  // Weak pull-down to 0
     assign osc_out = c;
